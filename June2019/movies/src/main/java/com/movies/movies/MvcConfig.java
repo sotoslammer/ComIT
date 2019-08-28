5af542c1518a2c8b3be4f9b9b6078cafa3c0b418
sotoslammer <desjarlais.brendan@gmail.com>
2019-08-28 17:35:20 -0600

    integrate github authentication

--- June2019/movies/src/main/java/com/movies/movies/MvcConfig.java
+++ June2019/movies/src/main/java/com/movies/movies/MvcConfig.java
@@ -10,8 +10,8 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
-        registry.addViewController("/login").setViewName("login");
+//        registry.addViewController("/hello").setViewName("hello");
+//        registry.addViewController("/login").setViewName("login");
     }
 
 }
--- June2019/movies/src/main/java/com/movies/movies/WebSecurityConfig.java
+++ June2019/movies/src/main/java/com/movies/movies/WebSecurityConfig.java
@@ -17,15 +17,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/", "/home", "/public/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+                .oauth2Login();
     }
 
     @Bean
