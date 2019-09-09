982ba2547d1740e92370fbee9cc5b7de81c5f377
sotoslammer <desjarlais.brendan@gmail.com>
2019-09-08 21:50:13 -0600

    test solutions

--- June2019/movies/src/test/java/com/movies/movies/ExerciseTest.java
+++ June2019/movies/src/test/java/com/movies/movies/ExerciseTest.java
@@ -8,16 +8,18 @@ public class ExerciseTest {
 
     @Test
     public void divide_returnsCorrec_whenNumbersAreEvenlyDivisible() {
-
+        int result = Exercise.divide(6, 3);
+        assertEquals(2, result);
     }
 
     @Test
     public void divide_returnsFloor_whenNumbersAreNotEvenlyDivisible() {
-
+        int result = Exercise.divide(7, 3);
+        assertEquals(2, result);
     }
 
-    @Test
+    @Test( expected = RuntimeException.class)
     public void divide_throwsException_whenDivisorIsZero() {
-
+        Exercise.divide(7, 0);
     }
 }
